c5fe3a329cf4f73f39bf6af23761288b96c304b9
samuel.johansson <samuel.johansson@PC2EMQWB>
2022-11-28 08:49:40 +0200

    Add project files.

---
+++ Harjoitus 1/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Harjoitus_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.otsikkoLB = new System.Windows.Forms.Label();
+            this.vaihdaBT = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // otsikkoLB
+            // 
+            this.otsikkoLB.AutoSize = true;
+            this.otsikkoLB.Location = new System.Drawing.Point(324, 95);
+            this.otsikkoLB.Name = "otsikkoLB";
+            this.otsikkoLB.Size = new System.Drawing.Size(47, 15);
+            this.otsikkoLB.TabIndex = 0;
+            this.otsikkoLB.Text = "Otsikko";
+            // 
+            // vaihdaBT
+            // 
+            this.vaihdaBT.Location = new System.Drawing.Point(324, 113);
+            this.vaihdaBT.Name = "vaihdaBT";
+            this.vaihdaBT.Size = new System.Drawing.Size(145, 50);
+            this.vaihdaBT.TabIndex = 1;
+            this.vaihdaBT.Text = "Vaihda teksti";
+            this.vaihdaBT.UseVisualStyleBackColor = true;
+            this.vaihdaBT.Click += new System.EventHandler(this.vaihdaBT_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.vaihdaBT);
+            this.Controls.Add(this.otsikkoLB);
+            this.Name = "Form1";
+            this.Text = "Harjoitus 1 | Samuel Johansson";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label otsikkoLB;
+        private Button vaihdaBT;
+    }
+}
---
+++ Harjoitus 1/Form1.cs
@@ -0,0 +1,15 @@
+namespace Harjoitus_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void vaihdaBT_Click(object sender, EventArgs e)
+        {
+            otsikkoLB.Text = "Heippa maailma";
+        }
+    }
+}
